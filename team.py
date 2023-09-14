d87a8d83524e119bb7bb51bcd1bfaf783597a3d7
Yoo-Ha-Young <bluekitten74@naver.com>
2023-09-14 12:18:03 +0900

    코드완성

--- team.py
+++ team.py
@@ -10,10 +10,10 @@ def f_sum(data):
 
 # 오름차순으로 정렬해주는 함수
 def f_sort(data):
-    #코드를 입력해 주세요
-    return 1
+    sorted_data = sorted(data)
+    return sorted_data
 
 # 내림차순으로 정렬해주는 함수
 def f_desc(data):
-    #코드를 입력해 주세요
-    return 1
+    sorted_data = sorted(data, reverse=True)
+    return sorted_data
