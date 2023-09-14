8fb0f18ae8e9a9394dd51b8509a4e5345a405744
kangsik10 <kstobit@naver.com>
2023-09-14 12:07:37 +0900

    b

---
+++ team.py
@@ -0,0 +1,19 @@
+# 평균을 구하는 함수
+def f_avg(data):
+    print("1")
+    return 1
+
+# 합계를 구하는 함수
+def f_sum(data):
+    #코드를 입력해 주세요
+    return 1
+
+# 오름차순으로 정렬해주는 함수
+def f_sort(data):
+    #코드를 입력해 주세요
+    return 1
+
+# 내림차순으로 정렬해주는 함수
+def f_desc(data):
+    #코드를 입력해 주세요
+    return 1
